2f89d3f26171052de1c46b2e754513e590474fdd
Sourav Dutta <138978234+SouravDutta22@users.noreply.github.com>
2025-05-16 19:22:09 +0530

    Update Login.jsx

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -4,6 +4,9 @@ const Login = ({handleLogin, toggleForm}) => {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
 
+    if(email==="admin@example.com" && password==="123"){
+        alert("Switch to admin login")
+    }
     const submitHandler = (e) => {
         e.preventDefault()
         handleLogin(email, password, false) // false indicates employee login
@@ -17,7 +20,7 @@ const Login = ({handleLogin, toggleForm}) => {
             onSubmit={(e) => {
                 submitHandler(e)
             }}
-            className='flex flex-col items-center justify-center'
+            className='flex flex-col items-center justify-center w-full'
             >
                 <input 
                 value={email}
@@ -25,7 +28,7 @@ const Login = ({handleLogin, toggleForm}) => {
                     setEmail(e.target.value)
                 }}
                 required 
-                className='outline-none text-gray-400 bg-transparent border-2 border-emerald-600 font-medium text-lg py-2 px-6 rounded-full w-full placeholder:text-gray-400' 
+                className='outline-none text-gray-400 bg-transparent border-2 border-emerald-600 font-medium text-base md:text-lg py-2 px-4 md:px-6 rounded-full w-full placeholder:text-gray-400' 
                 type="email" 
                 placeholder='Employee Email' 
                 />
@@ -35,18 +38,18 @@ const Login = ({handleLogin, toggleForm}) => {
                     setPassword(e.target.value)
                 }}
                 required 
-                className='text-gray-400 outline-none bg-transparent border-2 border-emerald-600 font-medium text-lg py-2 px-6 rounded-full mt-3 mb-5 w-full placeholder:text-gray-400' 
+                className='text-gray-400 outline-none bg-transparent border-2 border-emerald-600 font-medium text-base md:text-lg py-2 px-4 md:px-6 rounded-full mt-3 mb-4 md:mb-5 w-full placeholder:text-gray-400' 
                 type="password" 
                 placeholder='Employee Password' />
-                <button className='mt-2 text-white border-none outline-none hover:bg-emerald-700 font-semibold bg-emerald-600 text-lg py-2 px-8 w-full rounded-full transition-colors duration-300'>
+                <button className='mt-2 text-white border-none outline-none hover:bg-emerald-700 font-semibold bg-emerald-600 text-base md:text-lg py-2 px-6 md:px-8 w-full rounded-full transition-colors duration-300'>
                     Log in as Employee
                 </button>
-                <p className='mt-4 text-sm text-cyan-50'>
+                <p className='mt-4 text-xs md:text-sm text-cyan-50'>
                     New employee? 
                     <button 
                         type="button"
                         onClick={toggleForm} 
-                        className='text-emerald-600 font-medium ml-2 hover:underline'>
+                        className='text-emerald-600 font-medium ml-1 md:ml-2 hover:underline'>
                         Sign Up
                     </button>
                 </p>
