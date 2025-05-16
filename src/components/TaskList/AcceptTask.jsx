0dfd099a1b79bdcae81837a16b71d401cd972a23
Sourav Dutta <138978234+SouravDutta22@users.noreply.github.com>
2025-05-16 19:29:51 +0530

    Update AcceptTask.jsx

--- src/components/TaskList/AcceptTask.jsx
+++ src/components/TaskList/AcceptTask.jsx
@@ -2,24 +2,25 @@ import React from 'react'
 
 const AcceptTask = ({ data, index, onStatusChange }) => {
   return (
-    <div className='flex-shrink-0 h-full w-[300px] p-5 bg-yellow-400 rounded-xl'>
-      <div className='flex justify-between items-center'>
-        <h3 className='bg-red-600 text-sm px-3 py-1 rounded'>{data.category}</h3>
-        <h4 className='text-sm'>{data.taskDate}</h4>
+    <div className='flex-shrink-0 h-full min-w-[260px] sm:min-w-[300px] p-4 sm:p-5 bg-yellow-900 rounded-xl'>
+      <h3 className='text-xs sm:text-sm'>🔴{data.category}</h3>
+      <h2 className='mt-3 sm:mt-5 text-lg sm:text-xl font-semibold text-white'>{data.taskTitle}</h2>
+      <div className='max-w-[250px] sm:max-w-[280px] mt-2 sm:mt-4'>
+        <p className='text-xs sm:text-sm mt-2 mb-2 text-gray-300'>
+          {data.taskDescription}
+        </p>
       </div>
-      <h2 className='mt-5 text-2xl font-semibold text-black'>{data.taskTitle}</h2>
-      <p className='text-sm mt-2 text-black'>
-        {data.taskDescription}
-      </p>
-      <div className='flex justify-between mt-6'>
+      <h4 className='text-xs sm:text-sm'><span className='font-bold'>Date:</span> {data.taskDate}</h4>
+      <h4 className='text-xs sm:text-sm mt-1 mb-1'><span className='font-bold'>Deadline:</span> {data.deadline}</h4>
+      <div className='flex flex-col sm:flex-row justify-between mt-4 sm:mt-6 gap-2 sm:gap-0'>
         <button 
-          className='bg-green-500 rounded font-medium py-1 px-2 text-xs'
+          className='bg-green-950 rounded-xl font-medium py-1.5 sm:py-2 px-2 text-xs sm:text-sm sm:mr-2 cursor-pointer'
           onClick={() => onStatusChange(index, 'completed')}
         >
           Mark as Completed
         </button>
         <button 
-          className='bg-red-500 rounded font-medium py-1 px-2 text-xs'
+          className='bg-amber-950 rounded-xl font-medium py-1.5 sm:py-2 px-2 text-xs sm:text-sm cursor-pointer'
           onClick={() => onStatusChange(index, 'failed')}
         >
           Mark as Failed
